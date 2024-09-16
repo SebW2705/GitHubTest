dd0402708d0a730eaa7166d7e6196111100ffd8a
Sebastian Williams <sebastian.williams@ralphthoresby.com>
2024-09-16 10:45:37 +0100

    Test C#
    Added some simple C# for a test

---
+++ test.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Declare a string variable
+        string greeting = "Hello, World!";
+        
+        // Print the greeting
+        Console.WriteLine(greeting);
+        
+        // Declare and initialize an integer
+        int number = 42;
+        
+        // Print the number
+        Console.WriteLine("The number is: " + number);
+        
+        // A basic loop that doesn't do much
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("This is loop iteration: " + i);
+        }
+        
+        // End the program
+        Console.WriteLine("Program finished.");
+    }
+}
